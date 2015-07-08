266a6a8e4fc3ea337576557eb84ca7aab5e22bcb
Joseph Alves <databentpx@gmail.com>
2015-07-08 12:28:41 -0400

    Initial

---
+++ server/game/index.js
@@ -0,0 +1,27 @@
+var EventEmitter = require('events').EventEmitter;
+var game = new EventEmitter();
+module.exports = game;
+
+var questionPairs = [
+    [`What might you name your first child?`, `What is the name of your pet (or your favorite pet name)?`],
+    [`Who is your biggest role model?`, `Who is the last person you talked to?`],
+    [`Who is your favorite superhero?`, `Give Joe a nickname!`]
+];
+
+var questionPairIndex = 0;
+
+game.sendOutNewQuestions = function () {
+
+    game.emit('newQuestions', questionPairs[questionPairIndex]);
+
+    if (questionPairIndex + 1 === questionPairs.length) {
+        questionPairIndex = 0;
+    } else {
+        questionPairIndex = questionPairIndex + 1;
+    }
+
+};
+
+game.newResponse = response => game.emit('newResponse', response);
+game.playerJoined = () => game.emit('playerJoined');
+game.playerLeft = () => game.emit('playerLeft');
